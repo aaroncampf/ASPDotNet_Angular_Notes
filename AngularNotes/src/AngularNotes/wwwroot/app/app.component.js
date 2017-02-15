f35f9873b0acf89dadd78b4956a2697b58615c6c
Aaron Campf <aaroncampf@gmail.com>
2017-02-15 15:39:50 -0800

    Added Bootstrap

--- AngularNotes/src/AngularNotes/wwwroot/app/app.component.js
+++ AngularNotes/src/AngularNotes/wwwroot/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n\n<p>Header</p>\n<router-outlet></router-outlet>\n<p>Footer</p>\n"
+            template: "\n\n<nav class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">\n\t<div class=\"container\">\n\t\t<!-- Brand and toggle get grouped for better mobile display -->\n\n\t\t<div class=\"navbar-header\">\n\t\t\t<button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">\n\t\t\t\t<span class=\"sr-only\">Toggle navigation</span>\n\t\t\t</button>\n\n\t\t\t<a class=\"navbar-brand\" href=\"/\">Home</a>\n\t\t\t<ul class=\"nav navbar-nav\">\n\t\t\t\t<li><a href=\"Companies\">Companies</a></li>\n\t\t\t\t<li><a href=\"AboutUs\">Contact Us</a></li>\n\t\t\t\t<li>\n\t\t\t\t\n\t\t\t\t</li>\n\t\t\t</ul>\n\t\t</div>\n\t\t<!--<div class=\"pull-right\">\n\t\t\t<div class=\"ui-widget\">\n\t\t\t\t<label for=\"SearchItems\" style=\"color:white\">Search Items: </label>\n\t\t\t\t<input id=\"SearchItems\" size=\"50\">\n\t\t\t</div>\n\t\t</div>-->\n\t</div>\n</nav>\n<br /> <br /> \n<router-outlet></router-outlet>\n<p>Footer</p>\n"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- AngularNotes/src/AngularNotes/wwwroot/app/app.component.ts
+++ AngularNotes/src/AngularNotes/wwwroot/app/app.component.ts
@@ -1,10 +1,36 @@
 ﻿import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `
+	selector: 'my-app',
+	template: `
 
-<p>Header</p>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+			</button>
+
+			<a class="navbar-brand" href="/">Home</a>
+			<ul class="nav navbar-nav">
+				<li><a href="Companies">Companies</a></li>
+				<li><a href="AboutUs">Contact Us</a></li>
+				<li>
+				
+				</li>
+			</ul>
+		</div>
+		<!--<div class="pull-right">
+			<div class="ui-widget">
+				<label for="SearchItems" style="color:white">Search Items: </label>
+				<input id="SearchItems" size="50">
+			</div>
+		</div>-->
+	</div>
+</nav>
+<br /> <br /> 
 <router-outlet></router-outlet>
 <p>Footer</p>
 `
